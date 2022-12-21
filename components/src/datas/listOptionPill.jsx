6df0a6618d412ab2d689cbe76ed6809264912720
cega- <gatume.c@gmail.com>
2022-12-21 21:31:36 +0100

    Add files via upload

---
+++ components/src/datas/listOptionPill.jsx
@@ -0,0 +1,18 @@
+export const listOptionPill =
+[
+    'Affinité',
+    'Générosité',
+    'Affirmé',
+    'Génial',
+    'Déterminé',
+    'Aligné',
+    'Eloigné'
+]
+
+//https://www.newline.co/fullstack-react/30-days-of-react/day-15/
+//https://stackoverflow.com/questions/63378748/add-a-spinner-while-waiting-for-promise-to-resolve-in-react
+//https://www.npmjs.com/package/react-use-promise
+//https://upmostly.com/tutorials/how-to-handle-promises-in-react
+
+//https://blog.shahednasser.com/react-query-tutorial-for-beginners/
+//https://www.section.io/engineering-education/react-query-data-fetching-and-server-state-management/
